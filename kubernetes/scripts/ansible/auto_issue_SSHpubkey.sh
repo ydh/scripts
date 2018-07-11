91e497b159709ea3d85bb7120fcf926c70de509b
Alvin <shiyou.wang@outlook.com>
2018-07-11 10:45:20 +0800

    DELETE K8s dir

--- kubernetes/scripts/ansible/auto_issue_SSHpubkey.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-user="root"
-passwd="rootpasswd"
-
-for i in `cat /scripts/serverip.txt`;do
-    ./auto_ssh.sh $user $passwd $i
-done
--- kubernetes/scripts/node-hostname.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-HOST_NAME=$(echo $(ifconfig ens3 |awk 'NR==2{print $2 }'|cut -d ":" -f2) | sed 's/\./\-/g')
-
-hostnamectl set-hostname node-${HOST_NAME}
